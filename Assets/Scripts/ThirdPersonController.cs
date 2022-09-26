9da889ea2875fec0e0256fa6e26b5e00fa7eb6e5
Burak Şenses <buraksenseees@gmail.com>
2022-09-26 17:58:38 +0300

    Joystick Added.

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+   public FixedJoystick leftJoystick;
+   public FixedTouchField touchField;
+   private PlayerAnimator _animator;
+
+   private float _x;
+   private float _z;
+   private float _animTransitionSpeed = 10f;
+   private void Awake()
+   {
+      _animator = GetComponent<PlayerAnimator>();
+   }
+
+   private void Update()
+   {
+      if (leftJoystick.anan)
+      {
+         _animator.SetDirections(_x,_z);
+         _x = Mathf.Lerp(_x , Mathf.Round(leftJoystick.Horizontal),_animTransitionSpeed * Time.deltaTime);
+         _z = Mathf.Lerp(_z , Mathf.Round(leftJoystick.Vertical),_animTransitionSpeed * Time.deltaTime);
+      }
+   }
+}
